e9572a9c8faf896c2c5331d1957f78fdeed6b705
Gabriel Xavier Pereira <gabriel220060@ilum.cnpem.br>
2023-03-09 17:34:36 -0300

    Adicionei o documento do experimento 2

--- AlgoritmosGeneticos/funcoes.py
+++ AlgoritmosGeneticos/funcoes.py
@@ -1,1 +1,10 @@
- 
+def funcao_objetivo_cb(individuo):
+    """Computa qual é a função objetivo do problema de caixas binárias
+    
+    Args:
+        individuo: lista contendo os genes das caixas binárias
+        
+    Return:
+        Um valor representando a soma dos genes do indivíduo.
+    """
+    return sum(individuo) 
